e6a1d3f3375f021379940679fea39e7214b2666a
seungsooHa92 <43424322+seungsooHa92@users.noreply.github.com>
2020-12-01 17:49:06 +0900

    newIssueRegister > page.evaluate (TODO sleep)

--- testForNewIssueRegister.js
+++ testForNewIssueRegister.js
@@ -1,18 +1,18 @@
 const {accountInfo} = require('./credential_data');
 const puppeteer = require('puppeteer');
 
+/*
+for Time Setting
+*/
 let  yymmdd = (date)=>{
     let mm = date.getMonth() +1;
     let dd = date.getDate();
-
     return [date.getFullYear(), (mm>9 ? '':'0')+mm, (dd>9?'':'0')+dd].join('');
-
 }
 let hhmmss = function(date) {
     var hh = date.getHours();
     var mm = date.getMinutes();
     var ss = date.getSeconds();
-
     return [(hh>9 ? '' : '0') + hh,
             (mm>9 ? '' : '0') + mm,
             (ss>9 ? '' : '0') + ss,
@@ -21,14 +21,35 @@ let hhmmss = function(date) {
 let yymmddhhmmss = function(date) {
     return yymmdd(date) + hhmmss(date);
 };
+
+/**
+ * 
+ *  ----------------------------------------------------------------------------------------------------------------------
+ * 
+ *  @function handle_alert
+ *
+ *  @param page: _issueRegister 에서 받아온 page 객체 
+ *  @description
+ *  <pre>
+ *      Before alert pop up appear
+ *      handle alert accept operation 
+ *  </pre>
+ *  
+ *  -----------------------------------------------------------------------------------------------------------------------
+ */
+const handle_alert = async(page)=>{
+
+    page.on('dialog',async (dialog) => {
+        await dialog.accept();
+    }); 
+
+}
 const _issueRegister = async()=>{
 
     const browser = await puppeteer.launch({
         headless: false, 
     });
-
     const registerPage = await browser.newPage();
-
     await registerPage.goto('https://ims.tmaxsoft.com/tody/auth/login.do');
     await registerPage.type('#id',accountInfo._id,{delay:20});
 
@@ -71,25 +92,33 @@ const _issueRegister = async()=>{
     await registerPage.waitForTimeout('1000');
     
     // Module ComboBox (dropDown )
-
-    
-    await registerPage.select('#mainModuleCode','048');
-    await registerPage.waitForTimeout(600);
-    // handle alert TODO
-    registerPage.on('dialog',dialog => {
-        dialog.accept();
-    });
-    //await registerPage.click(`input[name='alert']`)​;
-
     /*
-    await registerPage.evaluate(()=>{
-        let init_y = 800;
-        for(let y = 0 ; y < 4 ; y++){
-            window.scrollTo(0,init_y+400*y);
+    ** important
+    handle_alert -> callback
+    callback must be precedent!
+    */
+    await handle_alert(registerPage);
+
+    const navigation3 = registerPage.waitForNavigation();
+    await registerPage.select('#mainModuleCode','048');
+    await navigation3;
+    await registerPage.waitForTimeout('600');
+
+    await registerPage.evaluate(async()=>{
+
+        const _sleep = async()=>{
+            return new Promise((resolve)=>{
+                setTimeout((resolve)=>{return resolve},1000);
+            })
         }
-        window.scrollTo(600,2000);
+ 
+        for(let y = 0 ; y < 10 ; y++){
+            window.scrollTo(0,150*y);
+            await _sleep();
+        }
+        window.scrollTo(600,150*9);
 
     })
-    */
+    
 }
 _issueRegister();
